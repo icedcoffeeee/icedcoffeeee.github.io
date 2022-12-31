999e40e3e510888f8195909b12bff32fbdab58b3
icedcoffeeee <83535735+icedcoffeeee@users.noreply.github.com>
2022-12-31 18:18:52 +0800

    presentation

---
+++ presentations/academic_perfum/assets/PresenterPrint-208d989d.js
@@ -0,0 +1,17 @@
+import{f as _,h as d,j as p,ag as u,c as m,ah as h,l as n,ai as t,aj as o,y as s,F as f,ak as g,al as v,am as x,n as r,an as y,ao as b,m as N,ap as k,aq as w,_ as P}from"./nav-fe86aae1.js";import{N as V}from"./NoteViewer-2f7f64b8.js";import{u as j}from"./index-81e0bc32.js";const S={class:"m-4"},C={class:"mb-10"},H={class:"text-4xl font-bold mt-2"},L={class:"opacity-50"},T={class:"text-lg"},B={class:"font-bold flex gap-2"},z={class:"opacity-50"},D=t("div",{class:"flex-auto"},null,-1),F={key:0,class:"border-gray-400/50 mb-8"},M=_({__name:"PresenterPrint",setup(q){d(p),u(`
+@page {
+  size: A4;
+  margin-top: 1.5cm;
+  margin-bottom: 1cm;
+}
+* {
+  -webkit-print-color-adjust: exact;
+}
+html,
+html body,
+html #app,
+html #page-root {
+  height: auto;
+  overflow: auto !important;
+}
+`),j({title:`Notes - ${m.title}`});const l=h(()=>x.slice(0,-1).map(a=>{var i;return(i=a.meta)==null?void 0:i.slide}).filter(a=>a!==void 0&&a.notesHTML!==""));return(a,i)=>(r(),n("div",{id:"page-root",style:v(s(w))},[t("div",S,[t("div",C,[t("h1",H,o(s(m).title),1),t("div",L,o(new Date().toLocaleString()),1)]),(r(!0),n(f,null,g(s(l),(e,c)=>(r(),n("div",{key:c,class:"flex flex-col gap-4 break-inside-avoid-page"},[t("div",null,[t("h2",T,[t("div",B,[t("div",z,o(e==null?void 0:e.no)+"/"+o(s(y)),1),b(" "+o(e==null?void 0:e.title)+" ",1),D])]),N(V,{"note-html":e.notesHTML,class:"max-w-full"},null,8,["note-html"])]),c<s(l).length-1?(r(),n("hr",F)):k("v-if",!0)]))),128))])],4))}}),R=P(M,[["__file","C:/GitHub/academic_perfum/academic perfum/node_modules/@slidev/client/internals/PresenterPrint.vue"]]);export{R as default};
